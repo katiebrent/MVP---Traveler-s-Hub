b93f1b6ff069819060bbae3931a3a1b1a33ae96a
Katie Brent <kab369@cornell.edu>
2021-05-20 10:19:35 -0600

    pushing to main before breaking everything again

--- Client/src/Map.jsx
+++ Client/src/Map.jsx
@@ -3,22 +3,27 @@ import { WorldMap } from "react-svg-worldmap";
 
 
 const TravelMap = (props) => {
+  const dataArr = [];
+
+
+  const [count, setCount] = useState(dataArr.length)
   const {countries} = props;
 
 
   // const defaultData = [{ country: "us", value: 1 }]
   console.log('this is countries data at travel Map', countries)
 
-  const dataArr = [];
 
   const dbData = countries.map((country) => {
       dataArr.push({"country": country.country_id,
       "value": 1})
   })
 
+
   const data = dataArr;
 
   console.log('this is countries data', data, Array.isArray(dataArr))
+
 if(dataArr.length < 1) {
   return (
     "You haven't visited anywhere yet! Start exploring or enter countries you have visited to see them on your map!"
@@ -26,7 +31,7 @@ if(dataArr.length < 1) {
 } else {
   return (
     <div>
-      <WorldMap color="dodgerblue" data={data} size="responsive" />
+      {dataArr.length % 2 === 0 ? <WorldMap color="dodgerblue" data={data} size="responsive" /> : <WorldMap color="dodgerblue" data={data} size="responsive" />}
     </div>
   )
 
